0163dec9e011d73e567a2d2d1811ceb346c9a334
James Futhey <futhey@gmail.com>
2015-05-31 13:57:50 -0700

    test

--- components/fill-murray/fill-murray.js
+++ components/fill-murray/fill-murray.js
@@ -1,40 +1,48 @@
-if (!window.Droplets) document.write('<script src="http://kidgodzilla.github.io/drop-in-web-components/library/loader.js"></script>');
+/**
+ *
+ */
+(function () {
 
-var interval = setInterval(function () {
-    if (window.Droplets) {
-        clearInterval(interval);
+    if (!window.Droplets) document.write('<script src="http://kidgodzilla.github.io/drop-in-web-components/library/loader.js"></script>');
 
-        Droplets.registerComponent({
-            name: "fill-murray",
-            afterRender: function (componentID) {
+    var tmp = (0|Math.random()*9e6).toString(36);
 
-                $(document).ready(function () {
+    window._DropletResolvers[tmp] = setInterval(function () {
+        if (window.Droplets) {
+            clearInterval(window._DropletResolvers[tmp]);
 
-                    $('<div class="fill-murray-component demo-component"></div>').insertAfter($('#' + componentID));
-                    $component = $(".fill-murray-component");
+            Droplets.registerComponent({
+                name: "fill-murray",
+                afterRender: function (componentID) {
 
-                    /**
-                     * Retrieve cached innerHTML for this component
-                     */
-                    var innerHTML = Droplets.getInnerHTML(componentID);
-                    $component.html(innerHTML);
+                    $(document).ready(function () {
 
-                    /**
-                     * Get & nice variables, set defaults, etc.
-                     */
-                    var width = $("#" + componentID).attr('data-width') || 200;
-                    var height = $("#" + componentID).attr('data-height') || 200;
-                    if (width) width = parseInt(width);
-                    if (height) height = parseInt(height);
-                    if (typeof(width) !== "number") width = 200;
-                    if (typeof(height) !== "number") height = 200;
+                        $('<div class="fill-murray-component demo-component"></div>').insertAfter($('#' + componentID));
+                        $component = $(".fill-murray-component");
 
-                    /**
-                     * Load Bill Murray on this component
-                     */
-                    $component.append('<img src="http://www.fillmurray.com/' + width + '/' + height + '/">');
-                });
-            }
-        });
-    }
-}, 100);
+                        /**
+                         * Retrieve cached innerHTML for this component
+                         */
+                        var innerHTML = Droplets.getInnerHTML(componentID);
+                        $component.html(innerHTML);
+
+                        /**
+                         * Get & nice variables, set defaults, etc.
+                         */
+                        var width = $("#" + componentID).attr('data-width') || 200;
+                        var height = $("#" + componentID).attr('data-height') || 200;
+                        if (width) width = parseInt(width);
+                        if (height) height = parseInt(height);
+                        if (typeof(width) !== "number") width = 200;
+                        if (typeof(height) !== "number") height = 200;
+
+                        /**
+                         * Load Bill Murray on this component
+                         */
+                        $component.append('<img src="http://www.fillmurray.com/' + width + '/' + height + '/">');
+                    });
+                }
+            });
+        }
+    }, 100);
+})();
