689ab31fd1a0a0271eef5a91e0af78e804ec3192
James Futhey <futhey@gmail.com>
2015-05-31 13:10:49 -0700

    test

--- components/fill-murray/fill-murray.js
+++ components/fill-murray/fill-murray.js
@@ -1,28 +1,35 @@
 Droplets.registerComponent({
-name: "fill-murray",
-afterRender: function (componentID) {
-$(document).ready(function () {
-/**
-* Retrieve cached innerHTML for this component
-*/
-var innerHTML = Droplets.getInnerHTML(componentID);
-$("#" + componentID + " .fill-murray-component").html(innerHTML);
+    name: "fill-murray",
+    afterRender: function (componentID) {
+        componentID = "DemoComponent"; // Test facilitation
+        $(document).ready(function () {
 
-/**
-* Get & nice variables, set defaults, etc.
-*/
-var width = $("#" + componentID).attr('data-width') || 200;
-var height = $("#" + componentID).attr('data-height') || 200;
-if (width) width = parseInt(width);
-if (height) height = parseInt(height);
-if (typeof(width) !== "number") width = 200;
-if (typeof(height) !== "number") height = 200;
+            $component = $('#' + componentID);
 
-/**
-* Load Bill Murray on this component
-*/
-$("#" + componentID + " .fill-murray-component").append('<img src="http://www.fillmurray.com/' + width + '/' + height + '/">');
+            $component.append('<div class="fill-murray-component"></div>');
+
+            $component = $("#" + componentID + " .fill-murray-component");
+
+            /**
+             * Retrieve cached innerHTML for this component
+             */
+            var innerHTML = Droplets.getInnerHTML(componentID);
+            $component.html(innerHTML);
+
+            /**
+             * Get & nice variables, set defaults, etc.
+             */
+            var width = $("#" + componentID).attr('data-width') || 200;
+            var height = $("#" + componentID).attr('data-height') || 200;
+            if (width) width = parseInt(width);
+            if (height) height = parseInt(height);
+            if (typeof(width) !== "number") width = 200;
+            if (typeof(height) !== "number") height = 200;
+
+            /**
+             * Load Bill Murray on this component
+             */
+            $component.append('<img src="http://www.fillmurray.com/' + width + '/' + height + '/">');
+        });
+    }
 });
-}
-});
-<div class="fill-murray-component"></div>
