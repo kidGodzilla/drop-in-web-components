6e866f053804ed1a0d4ae4d135f052cef21b8f47
James Futhey <futhey@gmail.com>
2015-05-31 19:23:16 -0700

    iso

--- components/isotope-filter/isotope-filter.js
+++ components/isotope-filter/isotope-filter.js
@@ -19,13 +19,50 @@ var cid = document.currentScript.id;
                 ID: componentID,
 
                 // END BOILERPLATE
-                render: function (componentID) {
+                render: function (componentID, $component) {
 
-                    $component = $('#' + componentID);
+                    $(document).ready(function () {
+                        /**
+                         * Retrieve cached innerHTML for this component
+                         */
+                        var innerHTML = Droplets.getInnerHTML(componentID);
+                        $component.html(innerHTML);
 
+                        /**
+                         * Load Isotope on this component
+                         */
+                        var $container = $component.isotope({
+                            itemSelector: '.element-item',
+                            layoutMode: 'fitRows',
+                            getSortData: {
+                                category: '[data-category]'
+                            }
+                        });
 
+                        /**
+                         * bind filter button click
+                         */
+                        $('#filters button').on('click', function () {
+                            $container.isotope({
+                                filter: $(this).attr('data-filter')
+                            });
+                        });
 
-                }
+                        /**
+                         * change is-checked & active classes on buttons
+                         */
+                        $('.button-group').each(function(i, buttonGroup) {
+                            var $buttonGroup = $(buttonGroup);
+                            $buttonGroup.on( 'click', 'button', function () {
+                                $buttonGroup.find('.is-checked, .active').removeClass('is-checked active');
+                                $(this).addClass('is-checked active');
+                            });
+                        });
+
+                    });
+
+                },
+                scripts: ['http://mfzy.co/isotope.pkgd.js']
             });
         }
     }
